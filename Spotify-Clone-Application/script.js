266fce69a8e44dacf8c9044593fef425dc718378
Abhinavsai-12 <abhinavsai1252@gmail.com>
2025-07-02 18:28:56 +0530

    Second commit

---
+++ Spotify-Clone-Application/script.js
@@ -0,0 +1,122 @@
+const songs = [
+  {
+    title: "Hanuman_Chalisa",
+    artist: "Krishna",
+    src: "assets/songs/hanuman_chalisa.mp3",
+    cover: "assets/songs/hanuman.jpg",
+  },
+  {
+    title: "Theme_Of_Kalki",
+    artist: "Narayanan",
+    src: "assets/songs/theme_of_kalki.mp3",
+    cover: "assets/songs/kalki.jpg",
+  },
+  {
+    title: "Mawaa_Enthaina",
+    artist: "Thaman",
+    src: "assets/songs/mawaa_enthaina.mp3",
+    cover: "assets/songs/gk.jpg",
+  },
+  {
+    title: "Samayama",
+    artist: "Narayanan",
+    src: "assets/songs/samayama.mp3",
+    cover: "assets/songs/hn.jpg",
+  },
+  
+
+
+
+
+  {
+    title: "Ta_Takkara",
+    artist: "Narayanan",
+    src: "assets/songs/ta_takkara_takka_takkara.mp3",
+    cover: "assets/songs/kalki.jpg",
+  },
+  {
+    title: "Ramana_Aei",
+    artist: "Thaman",
+    src: "assets/songs/ramana_aei.mp3",
+    cover: "assets/songs/gk.jpg",
+  },
+  {
+    title: "Odiyamma",
+    artist: "Santhosh",
+    src: "assets/songs/odiyamma.mp3",
+    cover: "assets/songs/hn.jpg",
+  },
+    {
+    title: "Raghunandana",
+    artist: "GowraHari",
+    src: "assets/songs/raghunandana.mp3",
+    cover: "assets/songs/hanuman.jpg",
+  },
+
+
+
+
+
+  {
+    title: "Dum_Masala",
+    artist: "Thaman",
+    src: "assets/songs/dum_masala.mp3",
+    cover: "assets/songs/gk.jpg",
+  },
+  {
+    title: "Glimpse",
+    artist: "Santhosh",
+    src: "assets/songs/glimpse_song.mp3",
+    cover: "assets/songs/hn.jpg",
+  },
+
+
+
+  // 2nd Copy
+    {
+    title: "Hanuman_Chalisa",
+    artist: "Krishna",
+    src: "assets/songs/hanuman_chalisa.mp3",
+    cover: "assets/songs/hanuman.jpg",
+  },
+  {
+    title: "Theme_Of_Kalki",
+    artist: "Narayanan",
+    src: "assets/songs/theme_of_kalki.mp3",
+    cover: "assets/songs/kalki.jpg",
+  },
+  {
+    title: "Mawaa_Enthaina",
+    artist: "Thaman",
+    src: "assets/songs/mawaa_enthaina.mp3",
+    cover: "assets/songs/gk.jpg",
+  },
+  {
+    title: "Samayama",
+    artist: "Narayanan",
+    src: "assets/songs/samayama.mp3",
+    cover: "assets/songs/hn.jpg",
+  },
+  
+
+
+
+
+  {
+    title: "Hanuman_Chalisa",
+    artist: "Krishna",
+    src: "assets/songs/hanuman_chalisa.mp3",
+    cover: "assets/songs/hanuman.jpg",
+  },
+  {
+    title: "Theme_Of_Kalki",
+    artist: "Narayanan",
+    src: "assets/songs/theme_of_kalki.mp3",
+    cover: "assets/songs/kalki.jpg",
+  },
+
+
+];
+
+
+
